060bba94247b14acf0dc8a665962512491fe8489
GeorgiNik <georgi.nik95@gmail.com>
2016-02-22 22:32:07 +0200

    order service made

---
+++ Source/Services/MvcTemplate.Services.Data/ClientService.cs
@@ -0,0 +1,29 @@
+﻿namespace MvcTemplate.Services.Data
+{
+    using System.Linq;
+
+    using MvcTemplate.Data.Common;
+    using MvcTemplate.Data.Models;
+
+    public class ClientService:IClientService
+    {
+        private IDbRepository<Client> clients;
+
+        private IDbRepository<Order> orders;
+
+        public IQueryable GetAll()
+        {
+            return this.clients.All();
+        }
+
+        public Client GetById(int id)
+        {
+            return this.clients.GetById(id);
+        }
+
+        public IQueryable GetAllOrders(int id)
+        {
+            return this.orders.All().Where(x => x.ClientId == id);
+        }
+    }
+}
---
+++ Source/Services/MvcTemplate.Services.Data/OrderService.cs
@@ -0,0 +1,47 @@
+﻿namespace MvcTemplate.Services.Data
+{
+    using System.Linq;
+
+    using MvcTemplate.Data.Common;
+    using MvcTemplate.Data.Models;
+
+    public class OrderService:IOrderService
+    {
+        private readonly IDbRepository<Order> orders;
+
+        public IQueryable GetAll()
+        {
+            return this.orders.All();
+        }
+
+        public Order GetById(int id)
+        {
+            return this.orders.GetById(id);
+        }
+
+        public IQueryable GetCompleted()
+        {
+            return this.orders.All().Where(x => x.IsComplited );
+        }
+
+        public IQueryable GetUnfinished()
+        {
+            return this.orders.All().Where(x => x.IsComplited == false);
+        }
+
+        public decimal GetOrdersTotalForMonth(int month)
+        {
+            return this.orders.All().Where(x => x.CreatedOn.Month == month).Sum(sum => sum.OrderPrice);
+        }
+
+        public IQueryable GetAllSentByEcont()
+        {
+            return this.orders.All().Where(x => x.Econt != 0);
+        }
+
+        public IQueryable GetAllPaidByCard()
+        {
+            return this.orders.All().Where(x => x.PaidWithCard != 0);
+        }
+    }
+}
