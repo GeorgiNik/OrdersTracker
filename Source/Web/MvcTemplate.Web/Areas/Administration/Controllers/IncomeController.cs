30260e2a3a9ef4c1abcbfa1de969bc10eccabbe1
GeorgiNik <georgi.nik95@gmail.com>
2016-02-24 13:41:47 +0200

    Income controller

---
+++ Source/Web/MvcTemplate.Web/Areas/Administration/Controllers/IncomeController.cs
@@ -0,0 +1,45 @@
+﻿namespace MvcTemplate.Web.Areas.Administration.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using MvcTemplate.Common;
+    using MvcTemplate.Services.Data.Contracts;
+    using MvcTemplate.Web.Infrastructure.Mapping;
+    using MvcTemplate.Web.ViewModels.Orders;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class IncomeController : Controller
+    {
+        private readonly IClientService clientService;
+
+        private readonly IOrderService orderService;
+
+        public IncomeController(IOrderService orderService, IClientService clientService)
+        {
+            this.orderService = orderService;
+            this.clientService = clientService;
+        }
+
+        public ActionResult Details()
+        {
+            return this.View();
+        }
+
+        public ActionResult Search(string query)
+        {
+            int month;
+            decimal result = 0M;
+            if (!int.TryParse(query, out month))
+            {
+                return this.PartialView("_SearchPartial", result);
+            }
+            var orders =
+                this.orderService.GetAll().To<OrdersViewModel>().Where(x => x.CreatedOn.Month == month).ToList();
+
+            result = orders.Sum(x => x.OrderPrice);
+
+            return this.PartialView("_SearchPartial", result);
+        }
+    }
+}
