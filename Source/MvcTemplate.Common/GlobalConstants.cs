dfa64e4f43e01c642e2e57235e66132cad5c6446
GeorgiNik <georgi.nik95@gmail.com>
2016-02-27 12:48:43 +0200

    stylecop fix

--- Source/MvcTemplate.Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MvcTemplate.Common
-{
-    public class GlobalConstants
-    {
-        public const string AdministratorRoleName = "Administrator";
-
-        public const int HomePageCacheDuration = 5 * 60;
-    }
-}
